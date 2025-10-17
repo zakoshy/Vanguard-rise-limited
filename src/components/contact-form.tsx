bee63563a22f99265c120e6b6c19b95ab7a9168b
Zakayo Oshome <zakayooshome254@gmail.com>
2025-10-17 10:36:11 +0000

    then the input section of the full name remove the placeholder let it be

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -79,7 +80,7 @@ export function ContactForm() {
                     <FormItem>
                         <FormLabel>Full Name</FormLabel>
                         <FormControl>
-                        <Input placeholder="John Doe" {...field} />
+                        <Input {...field} />
                         </FormControl>
                         <FormMessage />
                     </FormItem>
